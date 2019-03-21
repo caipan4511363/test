a55a6447ec4a0c8aef3a7f1e520249e5b009976a
caipan-rt <caipan-rt>
2019-03-21 09:53:01 +0800

    第一次

---
+++ java/src/Changshi.java
@@ -0,0 +1,20 @@
+/**
+ * FileName: Changshi
+ * Author:   Administrator
+ * Date:     2019/3/21 9:52
+ * History:
+ * <author>          <time>          <version>          <desc>
+ * 作者姓名           修改时间           版本号              描述
+ */
+
+/**
+ * @author caipan
+ * @create 2019/3/21
+ * @since 1.0.0
+ */
+public class Changshi {
+
+    public static void main(String[] args) {
+        System.out.println("111,hello!!");
+    }
+}
